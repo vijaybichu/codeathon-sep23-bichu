564e29a0ddaacbb33a167a8ea07b48660a2a4319
vijaybichu <134511865+vijaybichu@users.noreply.github.com>
2023-09-21 15:03:14 +0530

    Update issue2.py

--- program-1/issue2.py
+++ program-1/issue2.py
@@ -1,4 +1,13 @@
 def find_max_profit(prices):
+    """
+    Finds the maximum profit that can be made by buying and selling stocks.
+
+    Args:
+        prices (list): A list of stock prices.
+
+    Returns:
+        int: The maximum profit that can be made, or -1 if no profit can be made.
+    """
     if len(prices) < 2:
         return -1
     
